30418bf3f4140b7f31cded4e6410e2fc5ef27980
vamsikrishna <vamsibittu24@gmail.com>
2025-05-16 10:55:39 +0530

    removed admin panel

--- adminpanel/src/app/(admin)/dashboard/page.js
+++
@@ -1,8 +0,0 @@
-export default function DashboardPage() {
-  return (
-    <div className="flex flex-col items-center justify-center h-full">
-      <h1 className="text-2xl font-bold ">Coming Soon...</h1>
-
-    </div>
-  );
-}
--- adminpanel/src/components/client-side-custom-editor.js
+++
@@ -1,8 +0,0 @@
-// components/client-side-custom-editor.js
-'use client' // Required for App Router
-
-import dynamic from 'next/dynamic';
-
-const CustomEditor = dynamic(() => import('@/components/tiptap'), { ssr: false });
-
-export default CustomEditor;

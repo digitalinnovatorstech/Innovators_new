ad455eed0ebe53863b2e1541ea55dfc4a1de5713
vamsikrishna <vamsibittu24@gmail.com>
2025-05-16 13:18:24 +0530

    first commit

--- server/src/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createContact, getAllContacts } = require('../controllers/contactController');
-
-router.post('/create', createContact);
-router.get('/getAll', getAllContacts);
-
-module.exports = router;
--- server/src/routes/generalTalkRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const contactController = require('../controllers/generalTalkController');
-
-router.post('/create', contactController.createContact);
-router.get('/getAll', contactController.getAllContacts);
-
-module.exports = router;
